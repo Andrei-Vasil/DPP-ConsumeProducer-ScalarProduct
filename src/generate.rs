5db995613c71c8eaf9d4b124f6ae6da6ee8ba089
Andrei Vasil <andrei.vasil.271@gmail.com>
2022-11-05 15:46:31 +0200

    changed smth

--- src/generate.rs
+++ src/generate.rs
@@ -2,14 +2,16 @@ use rand::Rng;
 
 pub fn generate_size() -> usize {
     let mut rng = rand::thread_rng();
-    return rng.gen_range(1..500);
+    // return rng.gen_range(1..500);
+    return 200;
 }
 
 pub fn generate_vector(size: usize) -> Vec<i32> {
     let mut rng = rand::thread_rng();
     let mut v: Vec<i32> = vec![];
     for _ in 0..size {
-        v.push(rng.gen_range(0..10));
+        // v.push(rng.gen_range(0..10));
+        v.push(2);
     }
     return v;
 }
